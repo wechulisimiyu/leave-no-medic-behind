6c2fb9049f6210478c9f64c149fd475d6869a703
wechu07 <wechuli017@gmail.com>
2022-08-12 15:26:35 +0300

    ejs templating

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Serving at port ${port}`)
+})
