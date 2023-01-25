8a80539adb373223a40d8cc2b27fad334363908b
wechu07 <wechu017@gmail.com>
2023-01-25 15:36:52 +0300

    add support for local mongodb

--- config/db.js
+++ config/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true)
 
+dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/lnmb'
+
 const connectDB = async () => {
     try {
-      const db = await mongoose.connect(process.env.DB_URL)
+      const db = await mongoose.connect(dbUrl)
       console.log(`Database connected: ${db.connection.host}`)
     } catch (err) {
       console.error(err)
