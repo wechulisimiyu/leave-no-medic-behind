3e3a5b197779975a8b7023c02f1adb0d7bfa9855
wechu07 <wechu017@gmail.com>
2023-01-25 22:40:54 +0300

    update db connection local vs mongo atlas

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true)
 
-dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/lnmb'
+// if you wish to develop offline, pass 'mongodb://localhost:27017/lnmb' instead
+// of process.env.DB_URL
 
 const connectDB = async () => {
     try {
-      const db = await mongoose.connect(dbUrl)
+      const db = await mongoose.connect(process.env.DB_URL)
       console.log(`Database connected: ${db.connection.host}`)
     } catch (err) {
       console.error(err)
