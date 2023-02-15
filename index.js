11b2a019d8fcf978454b37d55812f0c55d586fce
wechu07 <wechu017@gmail.com>
2023-02-16 01:07:49 +0300

    adminjs cleanup

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const AdminJS = require('adminjs')
-const AdminJSExpress = require('@adminjs/express')
 const express = require('express')
+const jwt = require('jsonwebtoken');
 const path = require('path')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const connectDB = require('./config/db')
+const flash = require("connect-flash");
+const session = require("express-session")
 const ejsMate = require('ejs-mate')
+const MongoStore = require('connect-mongo');
 const methodOverride = require('method-override')
 const homeRoute = require('./src/routes/home')
 const lipaRoute = require('./src/routes/payment')
-const admin = require('./admin')
+const adminRoute = require('./src/routes/user')
 
 mongoose.set('strictQuery', true)
 
@@ -39,8 +41,40 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
 
+// const sessionStore = new MongoStore({
+//     url: process.env.DB_URL
+//   })
+
+app.use(session({
+    secret: 'my-secret',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.DB_URL,
+      }),
+    expires: new Date(Date.now() + (20 * 60 * 1000))
+  }));
+
+app.use(flash());
+
+
+app.use(async (req, res, next) => {
+    if (req.headers["x-access-token"]) {
+     const accessToken = req.headers["x-access-token"];
+     const { userId, exp } = await jwt.verify(accessToken, process.env.JWT_SECRET);
+     // Check if token has expired
+     if (exp < Date.now().valueOf() / 1000) { 
+      return res.status(401).json({ error: "JWT token has expired, please login to obtain a new one" });
+     } 
+     res.locals.loggedInUser = await User.findById(userId); next(); 
+    } else { 
+     next(); 
+    } 
+   });
+
 app.use("/", homeRoute)
 app.use("/payment", lipaRoute)
+app.use("/admin", adminRoute)
 
 const port = process.env.PORT || 4000
 
---
+++ public/js/slick.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+  $(".center").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
+
+$('.responsive').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
