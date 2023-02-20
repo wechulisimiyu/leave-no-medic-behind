7f6cd597cbb9c918f9791d184470d0132ea99e6c
wechu07 <wechu017@gmail.com>
2023-02-20 21:14:24 +0300

    unfinished: edit stkPush post route

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -4,7 +4,19 @@ const { initiateSTKPush, stkPushCallback, confirmPayment } = require('../control
 
 const { accessToken } = require('../middleware/generateAccessToken')
 
-router.route('/stkPush').post(accessToken, initiateSTKPush)
+// router.route('/stkPush').post(accessToken, )
+router.route("/stkPush").post(accessToken, async (req, res) => {
+    const amount = req.query.amount;
+    const phone = req.query.phone;
+  
+    try {
+      const response = await initiateSTKPush(amount, phone); // Call the function to initiate STK push
+      res.status(200).json(response);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error initiating STK push" });
+    }
+  });
 router.route('/stkPushCallback/:Order_ID').post(stkPushCallback)
 router.route('/confirmPayment/:CheckoutRequestID').post(accessToken, confirmPayment)
 
