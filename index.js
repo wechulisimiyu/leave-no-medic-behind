235db3c70f041ec18db4f33ab074753195c7b85f
wechu07 <wechu017@gmail.com>
2023-03-17 03:50:36 +0300

    add flash local for run register

--- index.js
+++ index.js
@@ -80,6 +80,7 @@ app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
+  res.locals.register = req.flash("register")
   next();
 });
 
