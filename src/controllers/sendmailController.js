8726196ba2e5788ff6a0f4fcd35cc99730c2a049
wechu07 <wechu017@gmail.com>
2023-04-06 03:03:29 +0300

    update mailing flow

--- src/controllers/sendmailController.js
+++ src/controllers/sendmailController.js
@@ -15,23 +15,9 @@ const transporter = nodemailer.createTransport({
 
 const mailOptions = {
   from: `"LNMB" <${process.env.SENDER_MAIL}>`,
-  to: process.env.TO_MAIL,
-  subject: 'Registration Confirmation - Leave no Medic Behind Run',
-  html: `<p>Dear Runner,</p>
-    <p>Thank you for registering for our annual "Leave no Medic Behind Run". We are thrilled to have you join us in supporting our cause to raise awareness and funds for medical students in need.</p>
-    <p>As a registered participant, you are now part of a community dedicated to making a positive impact in the lives of your fellow colleagues and future doctors. Your participation will help us in our efforts to provide financial assistance and resources to our student doctors who are in need of financial assistance.</p>
-    <p>You will be able to get your t-shirt from your chosen pick-up stations  from 19th April to 28th April (Monday- Friday from 11am to 3pm).</p>
-    <p>Here are some important details regarding the event:</p>
-    <ul>
-      <li>Date: 29th April 2023</li>
-      <li>Time: 7.30am</li>
-      <li>Location: KMTC Field Off Ngong Road</li>
-    </ul>
-    <p>Please arrive at least 30 minutes before the start time for registration and warm-up exercises. We encourage you to invite friends and family members to join you on this day to show support for our fellow medics.</p>
-    <p>We will all run with our LNMB t-shirts to show our solidarity as a tight knit community which cares for all our future medical professionals. In addition, we will have a water station at the halfway point of the run, and light refreshments will be available for purchase at the finish line.</p>
-    <p>Thank you again for your support, and we look forward to seeing you at the event!</p>
-    <p>Best regards,</p>
-    <p>2023 LNMB RUN ORGANIZING COMMITTEE</p>`
+  to: '',
+  subject: '',
+  html: ''
 }
 
 module.exports = { transporter, mailOptions }
---
+++ src/utils/mailMessageTemplates.js
@@ -0,0 +1,63 @@
+const runnerSignupMessage = `<p>Dear Runner,</p>
+<p>Thank you for registering for our annual "Leave no Medic Behind Run". We are thrilled to have you join us in supporting our cause to raise awareness and funds for medical students in need.</p>
+<p>As a registered participant, you are now part of a community dedicated to making a positive impact in the lives of your fellow colleagues and future doctors. Your participation will help us in our efforts to provide financial assistance and resources to our student doctors who are in need of financial assistance.</p>
+<p>You will be able to get your t-shirt from your chosen pick-up stations  from 19th April to 28th April (Monday- Friday from 11am to 3pm).</p>
+<p>Here are some important details regarding the event:</p>
+<ul>
+  <li>Date: 29th April 2023</li>
+  <li>Time: 7.30am</li>
+  <li>Location: KMTC Field Off Ngong Road</li>
+</ul>
+<p>Please arrive at least 30 minutes before the start time for registration and warm-up exercises. We encourage you to invite friends and family members to join you on this day to show support for our fellow medics.</p>
+<p>We will all run with our LNMB t-shirts to show our solidarity as a tight knit community which cares for all our future medical professionals. In addition, we will have a water station at the halfway point of the run, and light refreshments will be available for purchase at the finish line.</p>
+<p>Thank you again for your support, and we look forward to seeing you at the event!</p>
+<p>Best regards,</p>
+<p>2023 LNMB RUN ORGANIZING COMMITTEE</p>`;
+
+const donationMessage = `<p>Dear Donor,</p>
+<p>Thank you for choosing to donate to our cause. Your contribution will go a long way in helping us raise awareness and funds for medical students in need.</p>
+<p>We appreciate your generosity and support for our mission to provide financial assistance and resources to our student doctors who are in need of financial aid.</p>
+<p>You can check our website for more details about our cause and upcoming events.</p>
+<p>Thank you again for your support, and we look forward to your continued partnership with us!</p>
+<p>Best regards,</p>
+<p>2023 LNMB RUN ORGANIZING COMMITTEE</p>
+`;
+
+const vendorMessage = `<p>Dear Prospective Vendor,</p>
+<p>Thank you for signing up to sell your wares at our upcoming run. We appreciate your interest in participating and supporting our cause to raise awareness and funds for medical students in need.</p>
+<p>We are thrilled to have you as a vendor, and we look forward to working with you to make this event a success. Please note that we will be sending you more information via email on the logistics of the event, including set up and breakdown times.</p>
+<p>Please take the time to visit our website to learn more about our cause and upcoming events. Your contribution will make a significant impact in the lives of student doctors who are in need of financial assistance.</p>
+<p>Once again, thank you for your support, and we look forward to your continued partnership with us.</p>
+<p>Best regards,</p>
+<p>2023 LNMB RUN ORGANIZING COMMITTEE</p>
+`;
+
+const universityPartnerMessage = `<p>Dear Participant,</p>
+<p>Thank you for your interest in the upcoming charity run. If you are a student from Mount Kenya University, Kenyatta University, Egerton University or Jomo Kenyatta University of Agriculture and Technology, please note that you will sign up for the run and purchase t-shirts through your school's liaison.</p>
+<p>We have liaisons from each of these universities who will facilitate your sign-up and t-shirt purchase. You can find their contact information below, and they will be happy to assist you with any questions or concerns you may have.</p>
+<p>We apologize for any inconvenience this may cause, but we believe that this process will make it easier for you to participate in the event and purchase your t-shirts. Thank you for your understanding, and we look forward to seeing you at the charity run.</p>
+<ul>
+  <li>Egerton</li>
+  <ul>
+    <li>Hannah Njuguna 0759679960</li>
+  </ul>
+  <li>JKUAT</li>
+  <ul>
+    <li>Davis 0111 727518</li>
+    <li>Brenda 0746 120652</li>
+    <li>Winnie 0112331714</li>
+  </ul>
+  <li>MKU</li>
+  <ul>
+    <li>Lynet 0794019309</li>
+  </ul>
+  <li>KU</li>
+  <ul>
+    <li>Samson +254 736 867475</li>
+  </ul>
+</ul>
+<p>Best regards,</p>
+<p>2023 LNMB Charity Run Organizing Team</p>    
+`;
+
+module.exports = { runnerSignupMessage, donationMessage, vendorMessage, universityPartnerMessage };
