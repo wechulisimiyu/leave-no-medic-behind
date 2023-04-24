a63a1976ff3e2e7b37f172c1dbd3dd842bd1f41a
wechu07 <wechu017@gmail.com>
2023-04-24 11:30:11 +0300

    remove redundant function

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -177,20 +177,4 @@ const createVendor = async (req, res) => {
   }
 };
 
-const formatPhoneNumber = (phone) => {
-  // Remove all non-digit characters from the phone number
-  const digitsOnly = phone.replace(/\D/g, "");
-
-  // Check if the phone number starts with "254"
-  if (digitsOnly.startsWith("254")) {
-    return digitsOnly;
-  } else if (digitsOnly.length === 9 && digitsOnly.startsWith("7")) {
-    // If the phone number starts with "7" and is 9 digits long, assume it's a Kenyan number and add "254" at the beginning
-    return `254${digitsOnly}`;
-  } else {
-    // Otherwise, return null to indicate that the phone number is invalid
-    return null;
-  }
-};
-
 module.exports = { buyTshirt, donation, createVendor };
