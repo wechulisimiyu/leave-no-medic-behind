24141931c59039cb5b442e86233481311b826e35
wechu07 <wechu017@gmail.com>
2024-03-30 11:57:09 +0300

    remove cloudinaty for vendor

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,24 +1,24 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary')
-const dotenv = require('dotenv')
+// // const cloudinary = require('cloudinary').v2;
+// // const { CloudinaryStorage } = require('multer-storage-cloudinary')
+// const dotenv = require('dotenv')
 
-// loading the envariables
-dotenv.config()
+// // loading the envariables
+// dotenv.config()
 
-// Configuration 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET
-});
+// // Configuration 
+// // cloudinary.config({
+// //   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+// //   api_key: process.env.CLOUDINARY_KEY,
+// //   api_secret: process.env.CLOUDINARY_SECRET
+// // });
 
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: "Vendor",
-    format: async (req, file) => 'jpeg',
-    public_id: (req, file) => 'computed-filename-using-request',
-  },
-});
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: "Vendor",
+//     format: async (req, file) => 'jpeg',
+//     public_id: (req, file) => 'computed-filename-using-request',
+//   },
+// });
 
-module.exports = { cloudinary, storage }
+// module.exports = { cloudinary, storage }
